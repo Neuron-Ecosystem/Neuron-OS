0bd9b0505bc7ce5c5bade01b410c58da91c5a8e3
Neuron-Ecosystem <wertq6306@gmail.com>
2025-11-07 23:08:17 +0300

    Update desktop.js

--- src/desktop.js
+++ src/desktop.js
@@ -1,3 +1,8 @@
+/**
+ * src/desktop.js
+ * Рендеринг иконок на рабочем столе
+ */
+
 const Desktop = {
   apps: [
     { id: 'notes', name: 'Notes', icon: 'notes.svg', url: 'src/apps/notes.html' },
@@ -12,7 +17,7 @@ const Desktop = {
     const container = document.getElementById('icons-container');
     container.innerHTML = this.apps.map(app => `
       <div class="desktop-icon" ondblclick="WindowManager.open('${app.id}', '${app.name}', '${app.url}', '${app.icon}')">
-        <img src="assets/icons/${app.icon}" alt="${app.name}">
+        <img src="assets/icons/${app.icon}" alt="${app.name}" loading="lazy">
         <span>${app.name}</span>
       </div>
     `).join('');
